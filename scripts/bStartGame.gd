16629b8137f8e239c39ff3a7fe17545969a0c74c
0none <0none@cumallover.me>
2020-05-07 20:17:28 +0200

    Added Controls and funny pyrocynical haha image

--- scripts/bStartGame.gd
+++ scripts/bStartGame.gd
@@ -1,25 +1,26 @@
-extends Label
+extends Button
+
 
 export(Resource) var newscene;
 export(bool) var exitsGame;
 
 func _ready():
-	pass 
-
-
-func _process(delta):
+	var _bindButton = self.connect("pressed",self, "loadScene");
+	if _bindButton == 0:
+		print_debug("Button binding successful");
+	else:
+		print_debug("Button binding failed with code " + _bindButton);
 	pass
 
-func _input(event):
-#	print(event);
-	if event is InputEventMouseButton and event.is_pressed():
-		if exitsGame: get_tree().quit();
-		print_debug("Buttonpress detected at viewport pos", get_viewport().get_mouse_position());
+func loadScene():
+	if exitsGame:
+		print_debug("Exiting Game...");
+		get_tree().quit();
+	else:
 		print_debug("Changing Scene to " + newscene.get_path());
 		var _changescene = get_tree().change_scene(newscene.get_path());
 		if _changescene == 0:
 			print_debug("Success");
 		else:
 			print_debug("Failed to Change Scenes. Code " + _changescene);
-		
 	pass
