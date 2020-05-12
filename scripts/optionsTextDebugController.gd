a7405ff48f7eae2803f9062c39fd9280d218e108
0none <0none@cumallover.me>
2020-05-12 23:59:09 +0200

    Debug options

---
+++ scripts/optionsTextDebugController.gd
@@ -0,0 +1,12 @@
+extends RichTextLabel
+
+var mainMenuScene = "res://scenes/mainmenu.tscn";
+
+func _process(delta):
+	if Input.is_action_pressed("ui_menu"):
+		print_debug("ah fuck me");
+		get_tree().change_scene(mainMenuScene);
+	if Input.is_action_just_pressed("ui_accept"):
+		text = "UI accept";
+	elif Input.is_action_just_pressed("ui_cancel"):
+		text = "UI cancel"
