e53df69bcdfd5d2b3bab92ac3edd4c48413db6e2
0none <0none@cumallover.me>
2020-06-01 13:35:14 +0200

    Added level switch check

--- scripts/levelSelectController.gd
+++ scripts/levelSelectController.gd
@@ -9,5 +9,9 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
-		get_tree().change_scene(mainMenuScene);
+		var _loadResult = get_tree().change_scene(mainMenuScene);
+		if _loadResult == OK:
+			print_debug("Successfully switched level");
+		else:
+			print_debug("Switching levels failed. Errorcode : " + _loadResult);
 
