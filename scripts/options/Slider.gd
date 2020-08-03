268ac60213d0839fe76bb70df7311244790e06bd
0none <0none@cumallover.me>
2020-08-03 22:52:38 +0200

    Fix #16 and mostly fix #15

--- scripts/options/Slider.gd
+++ scripts/options/Slider.gd
@@ -1,24 +1,40 @@
 extends Node
 
-# Reference to the local slider node
+# Reference to the local label node
+var label;
 var slider;
 
 # Constants for range conversion
 var maxDB = 0;
 var minDB = -72;
 
+export var bus : int;
+
 func _ready():
 	for n in get_children():
 		if n.name == "value":
 			print_debug("found value label");
+			label = n;
+		elif n.name == "HSlider":
+			print_debug("found slider node");
 			slider = n;
-
-
-func _on_HSlider_value_changed(value,bus):
-	slider.text = str(value) + "%"
-	AudioServer.set_bus_volume_db(bus, convertRange(value));
 	
-# Convert 
-func convertRange(oldVal):
-	var newRange = (maxDB - minDB)
-	return ((oldVal * newRange ) / 100 ) + minDB;
+	# Set percentage to current volume
+	var currentVol = convertDbToPercent(AudioServer.get_bus_volume_db(bus));
+	slider.value = currentVol;
+	label.text = str(currentVol) + "%";
+	# Set slider to editable.
+	# It is disabled by default to avoid a race condition
+	slider.editable = true;
+
+func _on_HSlider_value_changed(value):
+	label.text = str(value) + "%"
+	AudioServer.set_bus_volume_db(bus, convertPercentToDb(value));
+	
+# Convert percent into dB range
+func convertPercentToDb(oldVal):
+	return ((oldVal * (maxDB - minDB) ) / 100 ) + minDB;
+
+# convert dB into Percent
+func convertDbToPercent(oldVal):
+	return (((oldVal - minDB) * 100) / (minDB*-1))
