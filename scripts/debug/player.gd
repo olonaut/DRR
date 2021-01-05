b2040ffa7c1a1fdeed7b5d68f63a17da39ecfcc3
0none <0none@cumallover.me>
2021-01-05 14:15:45 +0100

    Added movement blockers

--- scripts/debug/player.gd
+++ scripts/debug/player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 export var speed : int
 
+#func _ready():
+#	print_debug(get_axis_lock())
 
 func _physics_process(delta):
 	if Input.is_action_pressed("mv_left"):
@@ -10,4 +12,5 @@ func _physics_process(delta):
 	if Input.is_action_pressed("mv_right"):
 		move_and_slide(Vector2(speed,0)); # multiply with speed
 		#print_debug("right")
+	#print_debug(get_global_position())
 	#move_and_slide(Vector2(0,1));
