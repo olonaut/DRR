057482dd339a6495af875642a479972753d5e6b8
0none <0none@cumallover.me>
2021-06-06 15:44:54 +0200

    misc changes

--- scripts/level1.gd
+++ scripts/level1.gd
@@ -4,6 +4,7 @@ var music : Node;
 export var bgMusic : AudioStream;
 
 func _ready():
+	# play music
 	music = get_tree().root.get_node("BgMusic");
 	music.stop(); # this is technically not necessary but.. you know. just in case.
 	music.stream = bgMusic;
