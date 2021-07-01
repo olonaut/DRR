553a84910b87dcb45a108c9645ad38fea332694e
0none <0none@cumallover.me>
2021-07-01 23:30:05 +0200

    give playershot a nice little trail

---
+++ scripts/shotController.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+func _ready():
+	pass
+
+func _proccess():
+	pass
+
+
+
+func _on_RigidBody2D_body_entered(body):
+	print_debug("detected collision");
+	pass # Replace with function body.
