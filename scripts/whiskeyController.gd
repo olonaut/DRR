74ab75da2658e371207e60da226ef0586ab7b9ca
0none <0none@cumallover.me>
2021-07-03 23:32:40 +0200

    whiskey death and particle effect

--- scripts/whiskeyController.gd
+++ scripts/whiskeyController.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
+var explisionEffect : Node2D;
+var sprite : Sprite;
+
+func _ready():
+	explisionEffect = get_child(2)
+	sprite = get_child(0)	
+
 func hit():
-	# TODO Spawn explosion or something
+	linear_velocity = Vector2(0,0);
+	angular_velocity = 0.0;
+	explisionEffect.emitting = true;
+	sprite.free();
+	yield(get_tree().create_timer(1.25), "timeout")
 	queue_free();
-	pass
