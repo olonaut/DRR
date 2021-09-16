aa2efb6d4080e880b0c518b033a0eb90c2e69bac
0none <0none@cumallover.me>
2021-09-16 13:14:31 +0200

    displaying health works now

--- scripts/PlayerControler.gd
+++ scripts/PlayerControler.gd
@@ -4,21 +4,31 @@
 extends Node2D
 
 export var health : int;
-var _healthContainer;
 
+var healthContainer;
+var healthRes = preload('res://prefabs/Heart.tscn');
 var playerRes = preload('res://prefabs/Player.tscn');
 var activePlayer;
 
 func _ready():
-	_healthContainer = get_node("UIContainer/VBoxContainer/HealthContainer")
+	healthContainer = get_node("UIContainer/VBoxContainer/HealthContainer");
 	spawnPlayer();
+	updateHealth();
 
 func playerDied():
-	print_debug("player has died")
-	# todo deduct health
+	print_debug("player has died");
+	health -= 1;
+	updateHealth();
 	# todo respawn
 	
 func spawnPlayer():
-	var activePlayer = playerRes.instance()
-	add_child(activePlayer)	
+	var activePlayer = playerRes.instance();
+	add_child(activePlayer);
 
+func updateHealth():
+	for n in healthContainer.get_children():
+		healthContainer.remove_child(n);
+		n.queue_free();
+		
+	for n in health:
+		healthContainer.add_child(healthRes.instance());
