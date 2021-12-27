e44083edb81a8ba3ae382b632e490a41373450de
0none <0none@cumallover.me>
2021-12-27 13:31:51 +0100

    Fix wrong prefab

--- scripts/boss_controller.gd
+++ scripts/boss_controller.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 
 export var health : int;
-var bossPrefab = preload('res://prefabs/Player.tscn');
+var bossPrefab = preload('res://prefabs/Boss.tscn');
+var bossInstance;
 
 func start():
-	print_debug("Starting...")
-	pass
+	print_debug("Starting...");
+	bossInstance = bossPrefab.instance();
+	add_child(bossInstance);
+
